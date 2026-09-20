d6ad6bcb5fe25d4759f751c5f22f22f4d469b4e7
Subhiram <gurlinkasubhiram@gmail.com>
2026-09-20 11:32:55 +0530

    checking if it would work with secrets

--- src/procurement_agent/ui/app.py
+++ src/procurement_agent/ui/app.py
@@ -36,12 +36,25 @@ import logging
 import sys
 import uuid
 from typing import Any
-
+import os
 import streamlit as st
 from dotenv import load_dotenv
 from langgraph.types import Command
 
 load_dotenv()
+
+for key in [
+    "GROQ_API_KEY",
+    "MISTRAL_API_KEY",
+    "NVIDIA_API_KEY",
+    "GOOGLE_API_KEY",
+    "OPENROUTER_API_KEY",
+    "TAVILY_API_KEY",
+    "EXA_API_KEY",
+    "FIRECRAWL_API_KEY",
+]:
+    if key in st.secrets:
+        os.environ[key] = st.secrets[key]
 
 from procurement_agent.config import get_settings  # noqa: E402
 from procurement_agent.graph.build import (  # noqa: E402
